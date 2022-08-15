84e3fe99a14441a73f1be5c02846ef4b3c75e991
Fernando Santillan <ferdiego88@gmail.com>
2022-08-15 16:53:37 -0500

    marcadores mapbox

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styleUrls: ['./marcadores.component.css']
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit{
+
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 16;
+
+  center: [number, number] = [-79.028684,-8.111585];
+
 
   constructor() { }
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+  });
 
-  ngOnInit(): void {
+  // Agregar un elemento html personalizado como marcador
+    // const markerHtml: HTMLElement = document.createElement('div');
+    // markerHtml.innerHTML = 'Hola mundo'
+    // const marker = new mapboxgl.Marker({
+    //   element: markerHtml
+    // })
+
+    // Marcador HardCodeado
+    // const marker = new mapboxgl.Marker()
+    //   .setLngLat(this.center)
+    //   .addTo(this.mapa)
+    // ;
   }
 
+  irMarcador() {
+
+  }
+
+  agregarMarcador() {
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const newmarker = new mapboxgl.Marker( {
+      draggable: true,
+      color
+    })
+      .setLngLat(this.center)
+      .addTo(this.mapa)
+    ;
+  }
 }
