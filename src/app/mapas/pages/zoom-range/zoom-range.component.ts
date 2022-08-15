04d474802c799c6737bd8f2ea74dc33b5835350b
Fernando Santillan <ferdiego88@gmail.com>
2022-08-15 16:10:20 -0500

    Terminar Procesos

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,24 +1,33 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.css']
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
   zoomLevel: number = 16;
+
+  center: [number, number] = [-78.96087282197577,-8.091805679758075];
+
   constructor() {
     console.log('constructor', this.divMapa);
+  }
+
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', ()=>{});
+    this.mapa.off('zoomend', ()=>{});
+    this.mapa.off('move', ()=>{});
   }
 
   ngAfterViewInit(): void {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-78.96087282197577,-8.091805679758075],
+      center: this.center,
       zoom: this.zoomLevel
   });
 
@@ -30,6 +39,12 @@ export class ZoomRangeComponent implements AfterViewInit {
         if (this.mapa.getZoom() > 18) {
             this.mapa.zoomTo(18)
         }
+    })
+
+    this.mapa.on('move', (event) => {
+      const target = event.target;
+      const {lat, lng} = target.getCenter();
+      this.center = [lat,lng];
     })
 
   }
