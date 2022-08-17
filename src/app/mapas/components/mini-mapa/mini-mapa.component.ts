fc2a306c28d7aa70818da0bcd0f453062985f425
Fernando Santillan <ferdiego88@gmail.com>
2022-08-17 09:16:04 -0500

    miniMapa

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
   styleUrls: ['./mini-mapa.component.css']
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
 
+  @Input()lngLat: [number, number] = [0,0];
+  @ViewChild('mapa') divMapa!: ElementRef;
   constructor() { }
+  ngAfterViewInit(): void {
+    const mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+  });
 
-  ngOnInit(): void {
+  console.log(mapa);
+      new mapboxgl.Marker()
+          .setLngLat(this.lngLat)
+          .addTo(mapa)
+
   }
+
 
 }
